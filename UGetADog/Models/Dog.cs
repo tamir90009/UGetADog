d32c029c8f65f4a1bd9a11d377f614452996c52d
tamir ariel <tamir90009@gmail.com>
2019-07-12 16:01:07 +0300

    Add project files.

---
+++ UGetADog/Models/Dog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UGetADog.Models
+{
+    public class Dog
+    {
+        public int DogID { get; set; }
+        public string Name { get; set; }
+        public double Age { get; set; }
+        public string Race { get; set; }
+        public bool Trained { get; set; }
+        public bool Immune { get; set; }
+        public bool Castrated { get; set; }
+        public string Sex { get; set; }
+        public Giver Owner { get; set; }
+
+    }
+}
---
+++ UGetADog/Models/UGetADogDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace UGetADog.Models
+{
+    public class UGetADogDBContext : DbContext
+    {
+        public DbSet<Dog> Dogs { get; set; }
+        public DbSet<Giver> Givers { get; set; }
+        public DbSet<Adopter> Adopters { get; set; }
+
+   
+    }
+}
